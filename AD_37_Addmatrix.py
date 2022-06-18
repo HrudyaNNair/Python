ca4a153df8a93429e36332b44801b69217f2c620
HrudyaNNair <85451406+HrudyaNNair@users.noreply.github.com>
2022-06-18 13:44:05 +0530

    Create AD_37_Addmatrix.py

---
+++ AD_37_Addmatrix.py
@@ -0,0 +1,7 @@
+import numpy as mat
+mat1=mat.array([[1,2,3],[4,5,6],[7,8,9]])
+mat2=mat.array([[7,8,9],[1,2,3],[4,5,6]])
+sum=mat1+mat2
+print("First Matrix :\n",mat1)
+print("Second Matrix :\n",mat2)
+print("Sum Matrix : \n",sum)
